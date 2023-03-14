e6728418646477bf50ff6483d10be5db3e23dab4
huaqingjin <kevinjin0423@gmail.com>
2023-03-14 14:27:43 -0700

    add revision for PharStat

---
+++ PharStatR1/fix_simu_late.R
@@ -0,0 +1,68 @@
+setwd("/home/MyResearch/CFOs/")
+library(magrittr)
+library(parallel)
+source("Lateonset_utils.R")
+source("utilities.R")
+source("CFO_tox_utils.R")
+source("./fixScs2.R")
+
+
+nsimu <- 5000
+seeds <- 1:nsimu
+tau <- 3
+accrual <- 6
+tite.dist <- 2
+accrual.dist <- 1
+init.dose=1
+add.args=list(alp.prior=target, bet.prior=1-target, CV=0.95, suspend=F, crmCI.CV=0.80, impute.method=2)
+#   design: the phase I design, 1: CFO, 2: TITE-CRM, 3:TITE-BOIN
+
+idx <- 1
+for (idx in 1:8){
+p.true <- p.trues[[idx]]
+
+run.fn <- function(i){
+    set.seed(seeds[i]) #10
+    print(i)
+    cfo.res <- Simu.Fn(target, p.true, tau, cohortsize, ncohort, 
+                     accrual, tite.dist, accrual.dist, design=1, add.args=add.args)
+    naive.cfo.res <- CFO.simu.fn(target, p.true, ncohort=ncohort, cohortsize=cohortsize,
+                                init.level=1,  add.args=add.args)
+    crm.res <- Simu.Fn(target, p.true, tau, cohortsize, ncohort, 
+                     accrual, tite.dist, accrual.dist, design=2, add.args=add.args)
+    boin.res <- Simu.Fn(target, p.true, tau, cohortsize, ncohort, 
+                     accrual, tite.dist, accrual.dist, design=3, add.args=add.args)
+    ress <- list(
+                 cfo=cfo.res,
+                 crm = crm.res, 
+                 ncfo=naive.cfo.res,
+                 boin = boin.res, 
+                 paras=list(p.true=p.true, 
+                             add.args=add.args,
+                             target=target,
+                             ncohort=ncohort,
+                             cohortsize=cohortsize)
+        )
+    ress
+    
+}
+
+
+results <- mclapply(1:nsimu, run.fn, mc.cores=20)
+file.name <- paste0("./PharStatR1/results/", "Simu_withCFO", nsimu, "_fix2_",  idx, ".RData")
+save(results, file=file.name)
+
+crm.ress <- lapply(1:nsimu, function(i)results[[i]]$crm)
+boin.ress <- lapply(1:nsimu, function(i)results[[i]]$boin)
+cfo.ress <- lapply(1:nsimu, function(i)results[[i]]$cfo)
+ncfo.ress <- lapply(1:nsimu, function(i)results[[i]]$ncfo)
+sum.all <- list(
+                CFO = phase1.post.fn(cfo.ress),
+                nCFO = phase1.post.fn(ncfo.ress),
+                BOIN = phase1.post.fn(boin.ress),
+                CRM = phase1.post.fn(crm.ress)
+                )
+print(p.true)
+print(phase.I.pretty.tb(sum.all))
+}
+
---
+++ PharStatR1/rand_simu_late.R
@@ -0,0 +1,83 @@
+setwd("/home/MyResearch/CFOs/")
+library(magrittr)
+library(parallel)
+source("utilities.R")
+source("Lateonset_utils.R")
+source("CFO_tox_utils.R")
+
+
+target <- 0.20
+nsimu <- 5000
+mus <- c(0.37, 0.50, 0.66, 0.86)
+#mus <- c(0.26, 0.41, 0.56, 0.74)
+ncohort <- 12
+cohortsize <- 3
+ndose <- 7
+tau <- 3
+accrual <- 6
+tite.dist <- 2
+accrual.dist <- 1
+init.dose <- 1
+add.args <- list(alp.prior=target, bet.prior=1-target, CV=0.95, suspend=F, crmCI.CV=0.80, impute.method=2)
+
+diff <- 0.05
+diffs <- c(0.05, 0.07, 0.10, 0.15)
+
+
+for (diff in diffs){
+mu <- mus[which(diffs==diff)]
+
+# target = 0.2, Dose Level 6
+# 0.05: mu = 0.35
+# 0.07: mu = 0.49
+# 0.10: mu = 0.65
+# 0.15: mu = 0.85
+# target = 0.2, Dose Level 7
+# 0.05: mu = 0.37
+# 0.07: mu = 0.50
+# 0.10: mu = 0.66
+# 0.15: mu = 0.86
+# target = 0.3, Dose Level 7
+# 0.05: mu = 0.26
+# 0.07: mu = 0.41
+# 0.10: mu = 0.56
+# 0.15: mu = 0.74
+
+run.fn <- function(k){
+    set.seed(seeds[k])
+    print(k)
+    p.true.all <- gen.rand.doses(ndose, target, mu1=mu, mu2=mu)
+    p.true <- p.true.all$p.true
+    tmtd <- p.true.all$mtd.level
+
+    cfo.res <- Simu.Fn(target, p.true, tau, cohortsize, ncohort, 
+                     accrual, tite.dist, accrual.dist, design=1, add.args=add.args)
+    naive.cfo.res <- CFO.simu.fn(target, p.true, ncohort=ncohort, cohortsize=cohortsize, init.level=1,  add.args=add.args)
+    crm.res <- Simu.Fn(target, p.true, tau, cohortsize, ncohort, 
+                     accrual, tite.dist, accrual.dist, design=2, add.args=add.args)
+    boin.res <- Simu.Fn(target, p.true, tau, cohortsize, ncohort, 
+                     accrual, tite.dist, accrual.dist, design=3, add.args=add.args)
+
+    ress <- list(
+                 cfo=cfo.res,
+                 ncfo=naive.cfo.res,
+                 crm = crm.res, 
+                 boin = boin.res, 
+                 paras=list(p.true=p.true, 
+                         mtd=tmtd, 
+                         add.args=add.args,
+                         target=target,
+                         ncohort=ncohort,
+                         cohortsize=cohortsize)
+                 )
+    ress
+}
+
+
+seeds <- 1:nsimu
+file.name <- paste0("./PharStatR1/results/", "Simu_withCFO", nsimu, "_phi_", 100*target,  "_random_", 100*diff,  ".RData")
+print(file.name)
+results <- mclapply(1:nsimu, run.fn, mc.cores=20)
+print(post.process.random(results))
+save(results, file=file.name)
+}
